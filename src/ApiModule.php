62ede2d848eda6bf09b747f3910ef85daa71eb03
Jakub Hrášek <hrasekj@gmail.com>
2019-03-28 09:23:12 +0100

    Move logic from RestController to ApiModule

--- src/ApiModule.php
+++ src/ApiModule.php
@@ -3,18 +3,34 @@
 namespace damidev\api;
 
 use Yii;
-use yii\base\Application;
-use yii\base\BootstrapInterface;
-use yii\base\InvalidConfigException;
-use yii\helpers\StringHelper;
-use yii\helpers\Json;
+use yii\web\Application;
 use yii\web\Response;
+use yii\base\InvalidConfigException;
+use yii\filters\ContentNegotiator;
+use yii\filters\RateLimiter;
+use damidev\api\Serializer;
+use damidev\api\behaviors\VerbFilter;
 
 /**
  * @author Jakub Hrášek
  */
 class ApiModule extends Module
 {
+    /**
+     * @var array the configuration for creating the serializer that formats the response data.
+     */
+    public $serializer = [
+        'class' => Serializer::class
+    ];
+
+    /**
+     * @var array the configuration for creating authenticator
+     */
+    public $authenticator = [
+        'class' => \yii\filters\auth\HttpHeaderAuth::class,
+        'header' => 'Access-Token'
+    ];
+
     /**
      * @var string the class name of the [[identity]] object.
      */
@@ -28,13 +44,13 @@ class ApiModule extends Module
 
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function bootstrap($app)
     {
         // allow only one instance of API module
         if (self::$apiBootstrapped) {
-            throw new InvalidConfigException('This module must be bootstrapped only once.');
+            throw new InvalidConfigException('This module must be bootstrapped only once, use damidev\api\Module instead.');
         }
 
         self::$apiBootstrapped = true;
@@ -51,7 +67,7 @@ class ApiModule extends Module
             $id = $this->getUniqueId();
 
             // change app behavior only for requests to this module
-            if (StringHelper::startsWith($route, $id) === false) {
+            if (\yii\helpers\StringHelper::startsWith($route, $id) === false) {
                 return;
             }
 
@@ -77,5 +93,77 @@ class ApiModule extends Module
         });
 
         parent::bootstrap($app);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::class,
+                'formatParam' => null,
+                'languageParam' => null,
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                ],
+                'languages' => $this->acceptLanguages()
+            ],
+            'verbFilter' => [
+                'class' => VerbFilter::class,
+                'controllerActions' => [$this, 'controllerVerbs'],
+            ],
+            'authenticator' => $this->authenticator,
+            // TODO make it work
+            // 'rateLimiter' => [
+            //     'class' => RateLimiter::class,
+            // ]
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function afterAction($action, $result)
+    {
+        $result = parent::afterAction($action, $result);
+        return $this->serializeData($result);
+    }
+
+    /**
+     * Declares the allowed HTTP verbs.
+     * Please refer to [[VerbFilter::actions]] on how to declare the allowed verbs.
+     * @param  yii\base\Action $action
+     * @return array the allowed HTTP verbs.
+     */
+    protected function controllerVerbs($action)
+    {
+        if (method_exists($action->controller, 'verbs')) {
+            return $action->controller->verbs();
+        }
+        return [];
+    }
+
+    /**
+     * List of acceptable languages for response
+     * @return array
+     */
+    protected function acceptLanguages()
+    {
+        $params = Yii::$app->params['acceptLanguages'] ?? [Yii::$app->sourceLanguage];
+        return array_unique(array_merge(['en-US', 'cs-CZ'], $params));
+    }
+
+    /**
+     * Serializes the specified data.
+     * The default implementation will create a serializer based on the configuration given by [[serializer]].
+     * It then uses the serializer to serialize the given data.
+     * @param mixed $data the data to be serialized
+     * @return mixed the serialized data.
+     */
+    protected function serializeData($data)
+    {
+        return Yii::createObject($this->serializer)->serialize($data);
     }
 }
--- src/controllers/RestController.php
+++ src/controllers/RestController.php
@@ -2,38 +2,13 @@
 
 namespace damidev\api\controllers;
 
-use Yii;
-use yii\filters\auth\HttpHeaderAuth;
-use yii\filters\ContentNegotiator;
-use yii\filters\VerbFilter;
-use yii\filters\RateLimiter;
-use yii\web\Response;
-use damidev\api\Serializer;
-
 /**
  * RestController is the base class for RESTful API controller classes.
- *
- * For more info see [[\yii\rest\Controller]].
  *
  * @author Jakub Hrášek
  */
 abstract class RestController extends \yii\web\Controller
 {
-    /**
-     * @var array the configuration for creating the serializer that formats the response data.
-     */
-    public $serializer = [
-        'class' => Serializer::class
-    ];
-
-    /**
-     * @var array the configuration for creating authenticator
-     */
-    public $authenticator = [
-        'class' => HttpHeaderAuth::class,
-        'header' => 'Access-Token'
-    ];
-
     /**
      * {@inheritdoc}
      */
@@ -41,70 +16,12 @@ abstract class RestController extends \yii\web\Controller
 
 
     /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'contentNegotiator' => [
-                'class' => ContentNegotiator::class,
-                'formatParam' => null,
-                'languageParam' => '_lang',
-                'formats' => [
-                    'application/json' => Response::FORMAT_JSON,
-                ],
-                'languages' => $this->acceptLanguages()
-            ],
-            'verbFilter' => [
-                'class' => VerbFilter::class,
-                'actions' => $this->verbs(),
-            ],
-            'authenticator' => $this->authenticator,
-            // TODO make it work
-            // 'rateLimiter' => [
-            //     'class' => RateLimiter::class,
-            // ]
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function afterAction($action, $result)
-    {
-        $result = parent::afterAction($action, $result);
-        return $this->serializeData($result);
-    }
-
-    /**
      * Declares the allowed HTTP verbs.
      * Please refer to [[VerbFilter::actions]] on how to declare the allowed verbs.
      * @return array the allowed HTTP verbs.
      */
-    protected function verbs()
+    public function verbs()
     {
         return [];
-    }
-
-    /**
-     * List of acceptable languages for response
-     * @return array
-     */
-    protected function acceptLanguages()
-    {
-        $params = Yii::$app->params['acceptLanguages'] ?? [Yii::$app->sourceLanguage];
-        return array_unique(array_merge(['en-US', 'cs-CZ'], $params));
-    }
-
-    /**
-     * Serializes the specified data.
-     * The default implementation will create a serializer based on the configuration given by [[serializer]].
-     * It then uses the serializer to serialize the given data.
-     * @param mixed $data the data to be serialized
-     * @return mixed the serialized data.
-     */
-    protected function serializeData($data)
-    {
-        return Yii::createObject($this->serializer)->serialize($data);
     }
 }
