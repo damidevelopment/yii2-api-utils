c68763c3ee1eab1631174a5d56e470fb1485c351
Martin Štěpánek <mstepanek015@gmail.com>
2019-06-27 11:04:04 +0200

    Make properties read-only

--- src/UserAgent.php
+++ src/UserAgent.php
@@ -9,6 +9,18 @@ namespace damidevelopment\apiutils;
 use yii\base\BaseObject;
 use yii\web\BadRequestHttpException;
 
+/**
+ * UserAgent
+ *
+ * @property string $appName
+ * @property string $appVersion
+ * @property string $buildNumber
+ * @property string $bundleId
+ * @property int $os
+ * @property string $osVersion
+ * @property string $deviceType
+ * @property string $uid
+ */
 class UserAgent extends BaseObject
 {
     const OS_ANDROID = 0;
@@ -17,22 +29,22 @@ class UserAgent extends BaseObject
     /**
      * @var string
      */
-    public $appName;
+    private $_appName;
 
     /**
      * @var string
      */
-    public $appVersion;
+    private $_appVersion;
 
     /**
      * @var string
      */
-    public $buildNumber;
+    private $_buildNumber;
 
     /**
      * @var string
      */
-    public $bundleId;
+    private $_bundleId;
 
     /**
      * @var int
@@ -42,17 +54,17 @@ class UserAgent extends BaseObject
     /**
      * @var string
      */
-    public $osVersion;
+    private $_osVersion;
 
     /**
      * @var string
      */
-    public $deviceType;
+    private $_deviceType;
 
     /**
      * @var string
      */
-    public $uid;
+    private $_uid;
 
     public function __construct(string $userAgent, array $config = [])
     {
@@ -60,6 +72,75 @@ class UserAgent extends BaseObject
         $this->configure($userAgent);
     }
 
+
+    /**
+     * Configures UserAgent object by string provided in header
+     *
+     * {appName}/{appVersion}/{buildNumber}/{bundleId} {OS}/{OSversion} ({deviceType};{deviceId})
+     *
+     * Example:
+     * huntigApp/1.0.3/28/com.damidev.huntingApp Android/9.0.0 (HTC_DesireS_S510e;4e4c5141b7cb261e)
+     *
+     * @param string $userAgent UserAgent information
+     * @throws BadRequestHttpException
+     */
+    private function configure(string $userAgent): void
+    {
+        try {
+            $pattern = '#([a-zA-Z0-9 ]+)\/([a-zA-Z0-9. ]+)\/([a-zA-Z0-9 ]+)\/([a-zA-Z0-9 .]+) ([a-zA-Z0-9 ]+)\/([a-zA-Z0-9 .]+)\(([a-zA-Z0-9 _.]+);([a-zA-Z0-9 -]+)\)#';
+            preg_match($pattern, $userAgent, $matches);
+
+            $this->_appName = $matches[1];
+            $this->_appVersion = $matches[2];
+            $this->_buildNumber = $matches[3];
+            $this->_bundleId = $matches[4];
+
+            $this->setOs($matches[5]);
+            $this->_osVersion = $matches[6];
+
+            $this->_deviceType = $matches[7];
+            $this->_uid = $matches[8];
+        } catch (\Exception $e) {
+            throw new BadRequestHttpException(\Yii::t('errors', 'Invalid UserAgent'));
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getAppName(): string
+    {
+        return $this->_appName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAppVersion(): string
+    {
+        return $this->_appVersion;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBuildNumber(): string
+    {
+        return $this->_buildNumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBundleId(): string
+    {
+        return $this->_bundleId;
+    }
+
+    /**
+     * @param string $os
+     * @return UserAgent
+     */
     public function setOs(string $os): self
     {
         if ($os === 'Android') {
@@ -71,39 +152,35 @@ class UserAgent extends BaseObject
         return $this;
     }
 
+    /**
+     * @return int
+     */
     public function getOs(): int
     {
         return $this->_os;
     }
 
-    /** Configures UserAgent object by string provided in header
-     *
-     * {appName}/{appVersion}/{buildNumber}/{bundleId} {OS}/{OSversion} ({deviceType};{deviceId})
-     *
-     * Example:
-     * huntigApp/1.0.3/28/com.damidev.huntingApp Android/9.0.0 (HTC_DesireS_S510e;4e4c5141b7cb261e)
-     *
-     * @param string $userAgent UserAgent information
+    /**
+     * @return string
      */
-    private function configure(string $userAgent): void
+    public function getOsVersion(): string
     {
-        try {
-            $pattern = '#([a-zA-Z0-9 ]+)\/([a-zA-Z0-9. ]+)\/([a-zA-Z0-9 ]+)\/([a-zA-Z0-9 .]+) ([a-zA-Z0-9 ]+)\/([a-zA-Z0-9 .]+)\(([a-zA-Z0-9 _.]+);([a-zA-Z0-9 -]+)\)#';
-            preg_match($pattern, $userAgent, $matches);
-
-            $this->appName = $matches[1];
-            $this->appVersion = $matches[2];
-            $this->buildNumber = $matches[3];
-            $this->bundleId = $matches[4];
-
-            $this->setOs($matches[5]);
-            $this->osVersion = $matches[6];
-
-            $this->deviceType = $matches[7];
-            $this->uid = $matches[8];
-        } catch (\Exception $e){
-            throw new BadRequestHttpException(\Yii::t('errors', 'Invalid UserAgent'));
-        }
+        return $this->_osVersion;
     }
 
+    /**
+     * @return string
+     */
+    public function getDeviceType(): string
+    {
+        return $this->_deviceType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUid(): string
+    {
+        return $this->_uid;
+    }
 }
